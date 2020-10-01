1c7c82f4958594f39d8a0f761df51da94da6cece
Freddy Cuellar <facuellarg@unal.edu.co>
2020-10-01 03:37:09 -0500

    video call

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // import express from 'express'
 const express = require('express')
 let io = require('socket.io')
-({
-    path:'/webRTC'
-})
+    ({
+        path: '/webRTC'
+    })
 // import io from 'socket.io'
 
 const app = express()
@@ -19,32 +19,41 @@ const server = app.listen(port, () => console.log('sdhkal'))
 
 io.listen(server)
 
-const peers =io.of('/webrtcPeer')
+const peers = io.of('/webrtcPeer')
 
 const connectedPeers = new Map()
 
-peers.on('connection',socket =>{
+peers.on('connection', socket => {
     console.log(socket.id)
-    socket.emit('connection-success',{success:socket.id})
-    connectedPeers.set(socket.id,socket)
+    socket.emit('connection-success', { success: socket.id })
+    connectedPeers.set(socket.id, socket)
 
-    socket.on('offerOrAnswer',(data)=>{
-        for(const[socketID,socket]of connectedPeers.entries() ){
-            if(socketID != data.socketID){
-                socket.emit('offerOrAnswer',data.payload)
+    socket.on('offerOrAnswer', (data) => {
+        for (const [socketID, socket] of connectedPeers.entries()) {
+            if (socketID != data.socketID) {
+                socket.emit('offerOrAnswer', data.payload)
             }
         }
     })
-    socket.on('candidate',(data)=>{
-        for(const[socketID,socket]of connectedPeers.entries() ){
-            if(socketID != data.socketID){
-                socket.emit('candidate',data.payload)
+    socket.on('candidate', (data) => {
+        for (const [socketID, socket] of connectedPeers.entries()) {
+            if (socketID != data.socketID) {
+                socket.emit('candidate', data.payload)
             }
         }
     })
-
-    socket.on('disconnect',()=>{
-        console.log('disconnected')
+    
+    socket.on('disconnect', (reason) => {
+        for (const [socketID, socket_map] of connectedPeers.entries()) {
+            if (socketID != socket.id) {
+                socket_map.emit('disconnected',reason)
+            }
+        }
+        console.log('disconnected: ',reason)
         connectedPeers.delete(socket.id)
+        
     })
+})
+peers.on('disconnect',socket=>{
+    socket.emit('disconnect',{reason:"me fui"})
 })
--- src/App.jsx
+++ src/App.jsx
@@ -1,138 +1,159 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { useEffect } from 'react';
 import io from 'socket.io-client'
 import './App.css';
 
 function App() {
-  const localVideoRef = React.createRef()
-  const remoteVideoRef = React.createRef()
-  let textref,pc,socket
-  let candidates=[]
-  
-  useEffect(() => {
+  // const localVideoRef = React.createRef()
+  // const remoteVideoRef = React.createRef()
+  const localVideoRef = useRef()
+  const remoteVideoRef = useRef()
+  const [sdp, setSdp] = useState(null)
+  let textref = useRef()
+  let pc = useRef()
+  let socket = useRef()
 
-    socket=io(
+  useEffect(() => {
+    socket.current = io(
       '/webrtcPeer',
       {
-        path:'/webRTC',
-        query:{}
+        path: '/webRTC',
+        query: {}
       }
     )
-    socket.on('connection-success',success=>{
-      console.log(success)
-    })
-
-    socket.on('offerOrAnswer',(sdp)=>{
-      textref.value = JSON.stringify(sdp)
-      pc.setRemoteDescription(new RTCSessionDescription(sdp))
-    })
-    socket.on('candidate',(candidate)=>{
-      pc.addIceCandidate(new RTCIceCandidate(candidate))
-      // candidates.push(candidate)
-    })
-
-
-    // const pc_config = null
     const pc_config = {
-      "iceServers":[
+      "iceServers": [
         {
-          urls:'stun:stun.l.google.com:19302',
+          urls: 'stun:stun.l.google.com:19302',
         }
-    ]
+      ]
     }
-    pc = new RTCPeerConnection(pc_config)
-    pc.onicecandidate = (e)=>{
-      if (e.candidate){
-        sendToPeer('candidate',e.candidate)
+    pc.current = new RTCPeerConnection(pc_config)
+    socket.current.on('connection-success', success => {
+      console.log("succes", success)
+    })
+    pc.current.onicecandidate = (e) => {
+      if (e.candidate) {
+        sendToPeer('candidate', e.candidate)
       }
     }
+    socket.current.on('disconnected', (reason) => {
+      console.log("razon", reason)
+      //   alert(reason)
+      //   pc.current.close()
 
-    pc.oniceconnectionstatechange= (e) => {
+    })
+    socket.current.on('candidate', (candidate) => {
+      console.log("Entrando")
+      pc.current.addIceCandidate(new RTCIceCandidate(candidate))
+      // candidates.push(candidate)
+    })
+    // const pc_current.config = null
+
+    pc.current.oniceconnectionstatechange = (e) => {
       console.log(e)
     }
 
-    pc.onaddstream = (e)=>{
+    pc.current.onaddstream = (e) => {
       remoteVideoRef.current.srcObject = e.stream
     }
 
-    const constrains ={'video':true}
-    const succes = (stream) =>{
+    const constrains = { 'video': true }
+    const succes = (stream) => {
       window.localStream = stream
-      localVideoRef.current.srcObject=stream
-      pc.addStream(stream)
+      localVideoRef.current.srcObject = stream
+      pc.current.addStream(stream)
 
     }
-    const failure = (e) =>{
+    const failure = (e) => {
       console.log("get UserMedia error: ", e)
     }
     navigator.mediaDevices.getUserMedia(constrains)
-    .then(succes)
-    .catch(failure)
-  },);
+      .then(succes)
+      .catch(failure)
 
-  const sendToPeer =(messageType, payload)=>{
-    socket.emit(messageType,{
-      socketID:socket.id,
+    socket.current.on('offerOrAnswer', (sdp) => {
+      textref.value = JSON.stringify(sdp)
+      pc.current.setRemoteDescription(new RTCSessionDescription(sdp))
+      setSdp(sdp)
+    })
+
+  }, []);
+
+  const sendToPeer = (messageType, payload) => {
+    socket.current.emit(messageType, {
+      socketID: socket.current.id,
       payload,
     })
   }
-  
-    const createOffer =()=>{
-      console.log('offer')
-      pc.createOffer({offerToReciveVideo:1})
-        .then(sdp=>{
-          pc.setLocalDescription(sdp)
-          sendToPeer('offerOrAnswer',sdp)
-        },e=>{})
-    }
-    const createAnswer =()=>{
-      console.log('Answer')
-      pc.createAnswer({offerToReciveVideo:1})
-        .then(sdp=>{
-          pc.setLocalDescription(sdp)
-          sendToPeer('offerOrAnswer',sdp)
-        },e=>{})
-    }
-    // const setRemoteDescription =()=>{
-    //   const dest = JSON.parse(textref.value)
-    //   pc.setRemoteDescription(new RTCSessionDescription(dest))
-    // }
-    // const addCandidate=()=>{
-    //   // const candidate = JSON.parse(textref.value)
-    //   // console.log('adding candidate: ',candidate)
-    //   // pc.addIceCandidate(new RTCIceCandidate(candidate))
-    //   candidates.forEach(candidate=>{
-    //     console.log('adding candidate: ',candidate)
-    //     pc.addIceCandidate(new RTCIceCandidate(candidate))
-    //   })
-    // }
+
+  const createOffer = () => {
+    console.log('offer')
+    pc.current.createOffer({ offerToReciveVideo: 1 })
+      .then(sdp => {
+        pc.current.setLocalDescription(sdp)
+        sendToPeer('offerOrAnswer', sdp)
+      }, e => { })
+  }
+  const createAnswer = () => {
+    console.log('Answer')
+    pc.current.createAnswer({ offerToReciveVideo: 1 })
+      .then(sdp => {
+        pc.current.setLocalDescription(sdp)
+        sendToPeer('offerOrAnswer', sdp)
+      }, e => { })
+  }
+  // const setRemoteDescription =()=>{
+  //   const dest = JSON.parse(textref.value)
+  //   pc.setRemoteDescription(new RTCSessionDescription(dest))
+  // }
+  // const addCandidate=()=>{
+  //   // const candidate = JSON.parse(textref.value)
+  //   // console.log('adding candidate: ',candidate)
+  //   // pc.addIceCandidate(new RTCIceCandidate(candidate))
+  //   candidates.forEach(candidate=>{
+  //     console.log('adding candidate: ',candidate)
+  //     pc.addIceCandidate(new RTCIceCandidate(candidate))
+  //   })
+  // }
 
 
-  
+  // if (sdp) {
+  //   console.log("tenemos state")
+  // }
+  console.log("state :", sdp)
   return (
     <div className="App">
-      <video 
-      ref={localVideoRef} autoPlay
-      style={{
-        height:300,
-        width:300,
-        backgroundColor:'black',
-        margin:5,
-      }}
-      ></video>
-      <video 
-      ref={remoteVideoRef} autoPlay
-      style={{
-        height:300,
-        width:300,
-        backgroundColor:'black',
-        margin:5,
-      }}
-      ></video>
 
-      <button onClick={createOffer}>Offer</button>
-      <button onClick={createAnswer}>Answer</button>
-      <textarea ref={ref=>textref=ref} cols="30" rows="10"></textarea>
+
+      <video
+        ref={localVideoRef} autoPlay
+        style={{
+          height: 300,
+          width: 300,
+          backgroundColor: 'black',
+          margin: 5,
+        }}
+      ></video>
+      {sdp?<video
+        ref={remoteVideoRef} autoPlay
+        style={{
+          height: 300,
+          width: 300,
+          backgroundColor: 'black',
+          margin: 5,
+        }}
+      ></video>:
+        <br/>
+      }
+
+      {/* <button onClick={createAnswer}>Answer</button>
+      <button onClick={createOffer}>Offer</button> */}
+      {sdp ?
+        <button onClick={createAnswer}>Answer</button> :
+        <button onClick={createOffer}>Offer</button>
+      }
+      <textarea ref={ref => textref = ref} cols="30" rows="10"></textarea>
       {/* <button onClick={setRemoteDescription}>setRemoteDescription</button>
       <button onClick={addCandidate}>addCandidate</button> */}
     </div>
